57268cf71cab9e4f102519d6b923f945caf5d2bb
David Cuthell and Haneen Abukhater <david@>
2017-07-20 14:32:43 -0700

    add computer player functionality

--- js/scripts.js
+++ js/scripts.js
@@ -35,6 +35,7 @@ Game.prototype.roll = function(player){
   if(this.dice.length === 1 && this.dice[0].lastRoll === 1){
     player.tempScore = player.score;
     player.turnVal = 0;
+    console.log(rollResults);
     return rollResults;
   }
   if(this.dice.length !== 1){
@@ -46,17 +47,19 @@ Game.prototype.roll = function(player){
       player.score = 0;
       player.tempScore = 0;
       player.turnVal = 0;
+      console.log(rollResults);
       return rollResults;
     }
     for(i=0; i < this.dice.length; i++){
       if(this.dice[i].lastRoll === 1){
         player.tempScore = player.score;
         player.turnVal = 0;
+        console.log(rollResults);
         return rollResults;
       }
     }
   }
-
+  console.log(rollResults);
   player.tempScore += newRolls;
   if(player.tempScore >= 100){
     alert("You win!");
@@ -83,7 +86,36 @@ Player.prototype.setScore = function(val){
   this.score += val;
 }
 
+Game.prototype.smartComp = function(){
+  if((this.player2.tempScore - this.player2.score) <= 20){
+    return 1;
+  }else{
+    return 0;
+  }
+  return 6;
+}
+
 //UI
+function startComp(game){
+  $(".p2").hide();
+  while (game.player2.turnVal === 1) {
+    var smart = game.smartComp();
+    if(smart === 1){
+      $("#rollResult").text(game.roll(game.player2));
+      if(game.player2.turnVal === 0){
+        $(".p2").hide();
+        $(".p1").show();
+      }
+      $("#p2Score").text(game.player2.tempScore);
+    }else if(smart === 0){
+      game.hold(game.player2);
+      $("#p2Score").text(game.player2.score);
+      $(".p2").hide();
+      $(".p1").show();
+    }
+  }
+}
+
 $(document).ready(function(){
   var game = new Game();
 
@@ -107,6 +139,8 @@ $(document).ready(function(){
     if(game.player1.turnVal === 0){
       $(".p1").hide();
       $(".p2").show();
+      game.player2.turnVal = 1;
+      startComp(game);
     }
     $("#p1Score").text(game.player1.tempScore);
   });
@@ -116,6 +150,8 @@ $(document).ready(function(){
     $("#p1Score").text(game.player1.score);
     $(".p1").hide();
     $(".p2").show();
+    game.player2.turnVal = 1;
+    startComp(game);
   });
 
   $(".p2.roll").click(function () {
